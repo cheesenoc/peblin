5ca9b1efb424303c9b8c55a801cc8de9482ec1e5
cheesenoc <cheesenoc@gmail.com>
2016-02-04 00:23:40 +0100

    newlines added

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ static TextLayer *s_content_layer;
 static TextLayer *p_content_layer;
 static ContentIndicator *s_indicator;
 static Layer *s_indicator_up_layer, *s_indicator_down_layer;
+static GRect bounds;
 
 static char s_content[] = "\n0\n1\n3\n5\n10\n12\n13\n20\n22\n\n";
 static char p_content[] = "Bern,Hirschengraben\nNFB19 Elfenau\nS9 Blinzern-Köniz\n6 Bern,Bahnhof\nNFB19 Elfenau\nS9 Blinzern\n6 Bern,Bahnhof\nNFB19 Elfenau\nS9 Blinzern\n6 Bern, Bahnhof\n\n";
@@ -39,27 +40,40 @@ static void opendata_transport_callback(OpendataTransportInfo *info, OpendataTra
       // text_layer_set_text(s_text_layer, s_buffer);
       text_layer_set_text(s_content_layer, s_content);
       text_layer_set_text(p_content_layer, p_content);
+      GSize text_size = text_layer_get_content_size(s_content_layer);
+      layer_set_frame(text_layer_get_layer(s_content_layer),
+                      GRect(bounds.origin.x, bounds.origin.y, 45, text_size.h));
+      layer_set_frame(text_layer_get_layer(p_content_layer),
+                      GRect(bounds.origin.x+50, bounds.origin.y, 500, text_size.h));
+      scroll_layer_set_content_size(s_scroll_layer, text_size);
     }
       break;
     case OpendataTransportStatusNotYetFetched:
+      text_layer_set_text(s_content_layer, "\n");
       text_layer_set_text(p_content_layer, "\nStatusNotYetFetched");
       break;
     case OpendataTransportStatusBluetoothDisconnected:
+      text_layer_set_text(s_content_layer, "\n");
       text_layer_set_text(p_content_layer, "\nStatusBluetoothDisconnected");
       break;
     case OpendataTransportStatusPending:
-      text_layer_set_text(p_content_layer, "\nStatusPending");
+      text_layer_set_text(s_content_layer, "\n\n");
+      text_layer_set_text(p_content_layer, "\nStatus\nPending");
       break;
     case OpendataTransportStatusFailed:
+      text_layer_set_text(s_content_layer, "\n");
       text_layer_set_text(p_content_layer, "\nStatusFailed");
       break;
     case OpendataTransportStatusBadLocationsUrl:
+      text_layer_set_text(s_content_layer, "\n");
       text_layer_set_text(p_content_layer, "\nStatusBadLocationUrl");
       break;
     case OpendataTransportStatusBadStationboardUrl:
+      text_layer_set_text(s_content_layer, "\n");
       text_layer_set_text(p_content_layer, "\nStatusBadStationboardUrl");
       break;
     case OpendataTransportStatusLocationUnavailable:
+      text_layer_set_text(s_content_layer, "\n");
       text_layer_set_text(p_content_layer, "\nStatusLocationUnavailable");
       break;
   }
@@ -85,7 +99,7 @@ static void window_load(Window *window) {
   // layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+  bounds = layer_get_bounds(window_layer);
 
   s_scroll_layer = scroll_layer_create(bounds);
   scroll_layer_set_click_config_onto_window(s_scroll_layer, window);
@@ -130,8 +144,10 @@ static void window_load(Window *window) {
 
   s_content_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y, 45, 2000));
   p_content_layer = text_layer_create(GRect(bounds.origin.x+50, bounds.origin.y, 500, 2000));
-  text_layer_set_text(s_content_layer, s_content);
-  text_layer_set_text(p_content_layer, p_content);
+  // text_layer_set_text(s_content_layer, s_content);
+  // text_layer_set_text(p_content_layer, p_content);
+  text_layer_set_text(s_content_layer, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  text_layer_set_text(p_content_layer, "\nPeblin\nstartin'\n...");
   text_layer_set_text_alignment(s_content_layer, GTextAlignmentRight);
   text_layer_set_text_alignment(p_content_layer, GTextAlignmentLeft);
   text_layer_set_font(s_content_layer, fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS));
@@ -141,9 +157,8 @@ static void window_load(Window *window) {
   GSize text_size = text_layer_get_content_size(s_content_layer);
   layer_set_frame(text_layer_get_layer(s_content_layer),
                   GRect(bounds.origin.x, bounds.origin.y, 45, text_size.h));
-  GSize text_size2 = text_layer_get_content_size(p_content_layer);
   layer_set_frame(text_layer_get_layer(p_content_layer),
-                  GRect(bounds.origin.x+50, bounds.origin.y, 500, text_size2.h));
+                  GRect(bounds.origin.x+50, bounds.origin.y, 500, text_size.h));
   scroll_layer_set_content_size(s_scroll_layer, text_size);
 }
 
